aede492ef967b95433c5afa6fc2177fab4bf30b2
Steph <stepsen.dev@gmail.com>
2022-01-24 21:31:28 +0000

    updated link to be validated

--- client/src/pages/EntryFormPage.js
+++ client/src/pages/EntryFormPage.js
@@ -114,92 +114,122 @@ class EntryFormPage extends Component<CreateNewProps> {
     const { entry } = this.state;
     console.log(entry);
     return (
-      <Formik
-        initialValues={entry}
-        validate={(values) => {
-          const errors = {};
-          if (!values.title) {
-            errors.title = "Title is required";
-          }
-          if (!values.description) {
-            errors.description = "Description is required";
-          }
-          return errors;
-        }}
-        onSubmit={(values, { setSubmitting }) => {
-          console.log(values);
-          this.handleSubmit(values, setSubmitting, this);
-        }}
-        enableReinitialize
-      >
-        {({ isSubmitting }, formProps) => (
-          <Form>
-            <div className='flex flex-col mb-4 w-60'>
-              <label
-                className='mb-2 font-bold text-md text-grey-darkest'
-                htmlFor='title'
-              >
-                Title
-              </label>
-              <Field
-                className='border py-2 px-3 text-grey-darkest'
-                name='title'
-              />
-              <ErrorMessage name='title' component={Error} />
-            </div>
-            <div className='flex flex-col mb-4 w-2/3'>
-              <label
-                className='mb-2 font-bold text-md text-grey-darkest'
-                htmlFor='title'
-              >
-                Description
-              </label>
-              <Field
-                className='border py-2 px-3 text-grey-darkest'
-                name='description'
-              />
-              <ErrorMessage name='description' component={Error} />
-            </div>
-            <div className='flex flex-col mb-4 w-2/3'>
-              <label
-                className='mb-2 font-bold text-md text-grey-darkest'
-                htmlFor='link'
-              >
-                Link
-              </label>
-              <Field
-                className='border py-2 px-3 text-grey-darkest'
-                name='link'
-              />
-            </div>
-            <div className='flex flex-col mb-4 w-60'>
-              <label
-                htmlFor='repeatingTimes'
-                className='block font-bold text-gray-darkest'
-              >
-                Repeating times
-              </label>
-              <div className='mt-1 w-60'>
-                <Field as='select' name='repeatingTimes' className='w-60 h-8'>
-                  <option value={"onn"}>Please select</option>
-                  <option value={5}>5 times</option>
-                  <option value={10}>10 times</option>
-                  <option value={15}>15 times</option>
-                </Field>
-              </div>
-            </div>
+      <div className='container flex justify-center flex-col mx-auto mt-30'>
+        <Formik
+          initialValues={entry}
+          validate={(values) => {
+            const errors = {};
+            if (!values.title) {
+              errors.title = "Title is required";
+            }
+            if (!values.description) {
+              errors.description = "Description is required";
+            }
 
-            {/* <Field type='text' name='title' /> */}
-            <button
-              type='submit'
-              disabled={isSubmitting}
-              className=' hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded'
-            >
-              Save
-            </button>
-          </Form>
-        )}
-      </Formik>
+            if (values.link) {
+              try {
+                let u = new URL(values.link);
+              } catch (e) {
+                console.log(e);
+                errors.link = "No valid link";
+              }
+            }
+            return errors;
+          }}
+          onSubmit={(values, { setSubmitting }) => {
+            console.log(values);
+            this.handleSubmit(values, setSubmitting, this);
+          }}
+          enableReinitialize
+        >
+          {({ isSubmitting }, formProps) => (
+            <div className='mx-10'>
+              <h3 className='text-2xl text-gray-500 font-bold mb-10 mt-10'>
+                {" "}
+                {this.state && this.state.entry && this.state.entry.entryId
+                  ? "Edit entry"
+                  : "Create new entry"}
+              </h3>
+              <Form>
+                <div className='flex flex-col mb-4 w-60'>
+                  <label
+                    className='mb-2 font-bold text-md text-grey-darkest'
+                    htmlFor='title'
+                  >
+                    Title
+                  </label>
+                  <Field
+                    className='border-2 border-gray-500 rounded-lg py-2 px-3 text-grey-darkest'
+                    name='title'
+                  />
+                  <ErrorMessage name='title' component={Error} />
+                </div>
+                <div className='flex flex-col mb-4 w-2/3'>
+                  <label
+                    className='mb-2 font-bold text-md text-grey-darkest'
+                    htmlFor='description'
+                  >
+                    Description
+                  </label>
+                  <Field
+                    className='border-2 border-gray-500 rounded-lg py-2 px-3 text-grey-darkest'
+                    name='description'
+                  />
+                  <ErrorMessage name='description' component={Error} />
+                </div>
+                <div className='flex flex-col mb-4 w-2/3'>
+                  <label
+                    className='mb-2 font-bold text-md text-grey-darkest'
+                    htmlFor='link'
+                  >
+                    Link
+                  </label>
+                  <Field
+                    className='border-2 border-gray-500 rounded-lg py-2 px-3 text-grey-darkest'
+                    name='link'
+                  />
+                  <ErrorMessage name='link' component={Error} />
+                </div>
+                <div className='flex flex-col mb-4 w-60'>
+                  <label
+                    htmlFor='repeatingTimes'
+                    className='block font-bold text-gray-darkest'
+                  >
+                    Repeating times
+                  </label>
+                  <div className='mt-1 w-60 bg-gray-600 rounded-md border-6 border-gray-700'>
+                    <Field
+                      className='flex items-center p-2 bg-gray-600 rounded-md border-6 border-gray-700'
+                      as='select'
+                      name='repeatingTimes'
+                      className='w-60 h-8'
+                    >
+                      <option
+                        className='bg-gray-100 rounded-md border-6 border-gray-700'
+                        value={"onn"}
+                      >
+                        Please select
+                      </option>
+                      <option value={5}>5 times</option>
+                      <option value={10}>10 times</option>
+                      <option value={15}>15 times</option>
+                    </Field>
+                  </div>
+                </div>
+
+                {/* <Field type='text' name='title' /> */}
+                <button
+                  type='submit'
+                  disabled={isSubmitting}
+                  className=' hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded'
+                >
+                  {isSubmitting ? "Saving ... " : "Save"}
+                </button>
+              </Form>
+            </div>
+          )}
+        </Formik>
+      </div>
     );
   }
 }
